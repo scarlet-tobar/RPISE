de76ccc3f32cebd0c4c48bcee26e0bf7d81a3667
scarlet-tobar <scarlet.tobar@alumnos.ucn.cl>
2023-06-01 11:14:23 -0400

    Función sacarFoto()

--- test.py
+++ test.py
@@ -2,7 +2,12 @@ import picamera
 from datetime import datetime
 import cv2
 import numpy as np
-camara = picamera.PiCamera()
-now = datetime.now()
-camara.capture(now.strftime("%Y-%m-%d-%H-%M-%S")  +".jpeg")
-camara.close()
+
+def sacarFoto():
+	camara = picamera.PiCamera()
+	now = datetime.now()
+	foto=camara.capture(now.strftime("%Y-%m-%d-%H-%M-%S")  +".jpeg")
+	camara.close()
+	return foto
+	
+sacarFoto()
