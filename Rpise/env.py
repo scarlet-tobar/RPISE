a8e41b41dc1348a26902b63133abc8dad556934d
Pedro Cuadra <pedrxcuadra@gmail.com>
2023-06-13 17:01:19 -0400

    CHANGE: valve & ac_light pins

--- Rpise/env.py
+++ Rpise/env.py
@@ -5,8 +5,8 @@ CONSTANTES. Modificar a gusto
 WS_SERVER_URL = "ws://localhost:8000" 
 
 GPIO_OUT_PINS = {
-    "VALVE" : 2,
-    "AC_LIGHT" : 3,
+    "VALVE" : 14,
+    "AC_LIGHT" : 15,
     "CAM_LIGHT" : 21   
 }
 
