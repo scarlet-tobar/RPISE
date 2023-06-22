93fde84836ac8a1f7598bc3888b43244c9b9d881
Pedro Cuadra <pedrxcuadra@gmail.com>
2023-06-22 10:43:29 -0400

    Change pin config

--- Rpise/env.py
+++ Rpise/env.py
@@ -5,9 +5,10 @@ CONSTANTES. Modificar a gusto
 WS_SERVER_URL = "ws://localhost:8000" 
 
 GPIO_OUT_PINS = {
-    "VALVE" : 14,
-    "AC_LIGHT" : 15,
-    "CAM_LIGHT" : 21   
+    "VALVE" : 17,
+    "AC_LIGHT" : 18,
+    "CAM_LIGHT_1" : 22,
+    "CAM_LIGHT_2" : 23   
 }
 
 ESTANQUE = 1
