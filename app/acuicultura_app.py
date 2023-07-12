90e8b00ab42fb11bdf5951bc5629f24f8e84a366
Miguel Gomez <miguel.gomez@noirlab.edu>
2023-07-12 09:46:31 -0400

    no idea

--- app/acuicultura_app.py
+++ app/acuicultura_app.py
@@ -1,1 +1,12 @@
+from db.db_connection import postgres_connection
+
+connection = postgres_connection()
+
+if connection is not None:
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM medicion")
+    connection.close()
+    print('Conexión cerrada')
+else:
+    print('No se pudo establecer la conexión a la base de datos')
 
