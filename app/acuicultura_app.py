238ad46603147d35f1f951a23737c88e0cd49106
Miguel Gómez <miguel.gomez@alumnos.ucn.cl>
2023-07-16 00:31:05 -0400

    nueva forma

--- app/acuicultura_app.py
+++ app/acuicultura_app.py
@@ -1,20 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 from db.db_connection import postgres_connection
 
 app = Flask(__name__)
 connection = postgres_connection()
 
 @app.route('/set/horario', methods=['POST'])
-def set_horario(id_estanque, hora_inicio, hora_termino):
-    print('horario inicio:', hora_inicio)
-    print('horario_termino:', hora_termino)
+def set_horario():
+    id_estanque = request.form.get('id_estanque')
+    hora_inicio = request.form.get('hora_inicio')
+    hora_termino = request.form.get('hora_termino')
+
     if connection is not None:
         cursor = connection.cursor()
-        cursor.execute("UPDATE estanque set hora_encendido=%s ,hora_apagado= %s where id_estanque =%s", (hora_inicio, hora_termino, id_estanque))
-        print('se cambio el horario de: ', id_estanque)
+        cursor.execute("UPDATE estanque SET hora_encendido = %s, hora_apagado = %s WHERE id_estanque = %s", (hora_inicio, hora_termino, id_estanque))
+        connection.commit()
+        print('se cambió el horario de:', id_estanque)
     else:
         print('No se pudo establecer la conexión a la base de datos')
 
+    return "Horario actualizado"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2000)
