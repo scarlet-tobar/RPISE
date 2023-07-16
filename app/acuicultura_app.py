7290326b6beee443abbbd03235416e7c6a862d48
Miguel Gómez <miguel.gomez@alumnos.ucn.cl>
2023-07-16 17:24:18 -0400

    refactoring

--- app/acuicultura_app.py
+++ app/acuicultura_app.py
@@ -7,26 +7,49 @@ from utils.env import GPIO_OUT_PINS
 app = Flask(__name__)
 connection = postgres_connection()
 
+current_id_estanque = None
+current_hora_inicio = None
+current_hora_termino = None
+current_luz = None
+
 @app.route('/set/horario', methods=['POST'])
 def set_horario():
-    data= request.json
-    id_estanque = data['id_estanque']
-    hora_inicio = data['hora_inicio']
-    hora_termino = data['hora_termino']
+    data = request.json
+    id_estanque = data.get('id_estanque')
+    hora_inicio = data.get('hora_inicio')
+    hora_termino = data.get('hora_termino')
+    
+    if id_estanque == current_id_estanque and hora_inicio == current_hora_inicio and hora_termino == current_hora_termino:
+        return "Datos iguales, no se realiza la actualización"
+    
     cursor = connection.cursor()
     cursor.execute("UPDATE estanque SET hora_encendido = %s, hora_apagado = %s WHERE id_estanque = %s", (hora_inicio, hora_termino, id_estanque))
     connection.commit()
-    print('se cambió el horario de:', id_estanque)
+    
+    global current_id_estanque, current_hora_inicio, current_hora_termino
+    current_id_estanque = id_estanque
+    current_hora_inicio = hora_inicio
+    current_hora_termino = hora_termino
+    
+    print('Se cambió el horario de:', id_estanque)
     return "Horario actualizado"
 
 @app.route('/set/luz', methods=['POST'])
 def set_luz():
-    data= request.json
-    luz=data["luz"] #debe ser bool
-    gpio.named_output("AC_LIGHT",bool(luz))
-    #CA.enviarEstadoAgua()
-    print('se cambió la luz a: '+ luz)
-    return "luz actualizada"
+    data = request.json
+    luz = data.get("luz")  # debe ser bool
+    
+    if luz == current_luz:
+        return "Datos iguales, no se realiza la actualización"
+    
+    gpio.named_output("AC_LIGHT", bool(luz))
+    # CA.enviarEstadoAgua()
+    
+    global current_luz
+    current_luz = luz
+    
+    print('Se cambió la luz a:', luz)
+    return "Luz actualizada"
 
 if __name__ == '__main__':
     if connection is not None:
