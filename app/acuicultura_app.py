2a941b09032c94fe68c29ee91af136a28804d0f1
Miguel Gomez <miguel.gomez@noirlab.edu>
2023-07-18 19:31:21 -0400

    test flask caching

--- app/acuicultura_app.py
+++ app/acuicultura_app.py
@@ -1,16 +1,17 @@
 from flask import Flask, request
+from flask_caching import Cache
 from db.db_connection import postgres_connection
 import utils.colorAgua as CA
 import utils.gpio as gpio
 from utils.env import GPIO_OUT_PINS
 
 app = Flask(__name__)
+cache = Cache(app)
 connection = postgres_connection()
 
-current_id_estanque = None
-current_hora_inicio = None
-current_hora_termino = None
-current_luz = None
+# Configuración de Flask-Caching
+app.config['CACHE_TYPE'] = 'simple'  # Puedes usar otros tipos de caché según tus necesidades
+cache.init_app(app)
 
 @app.route('/set/horario', methods=['POST'])
 def set_horario():
@@ -19,9 +20,19 @@ def set_horario():
     hora_inicio = data.get('hora_inicio')
     hora_termino = data.get('hora_termino')
 
+    # Comprobar si los valores son iguales a los almacenados en caché
+    cache_key = f'horario_{id_estanque}'
+    cached_horario = cache.get(cache_key)
+
+    if cached_horario and cached_horario.get('hora_inicio') == hora_inicio and cached_horario.get('hora_termino') == hora_termino:
+        return "El horario es el mismo, no se realizó ningún cambio"
+
     cursor = connection.cursor()
     cursor.execute("UPDATE estanque SET hora_encendido = %s, hora_apagado = %s WHERE id_estanque = %s", (hora_inicio, hora_termino, id_estanque))
     connection.commit()
+
+    # Actualizar los valores en caché
+    cache.set(cache_key, {'hora_inicio': hora_inicio, 'hora_termino': hora_termino})
 
     print('Se cambió el horario de:', id_estanque)
     return "Horario actualizado"
@@ -29,12 +40,23 @@ def set_horario():
 @app.route('/set/luz', methods=['POST'])
 def set_luz():
     data = request.json
-    luz = data.get("luz")  # debe ser bool
+    id_estanque = data.get('id_estanque')
+    luz = data.get('luz')
+
+    # Comprobar si el valor es igual al almacenado en caché
+    cache_key = f'luz_{id_estanque}'
+    cached_luz = cache.get(cache_key)
+
+    if cached_luz == luz:
+        return "La luz es la misma, no se realizó ningún cambio"
 
     gpio.named_output("AC_LIGHT", bool(luz))
     # CA.enviarEstadoAgua()
 
-    print('Se cambió la luz a:', luz)
+    # Actualizar el valor en caché
+    cache.set(cache_key, luz)
+
+    print('Se cambió la luz de:', id_estanque)
     return "Luz actualizada"
 
 @app.route('/set/medicion', methods=['POST'])
