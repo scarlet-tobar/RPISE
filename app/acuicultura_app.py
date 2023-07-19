515a15a8a38a5977ca2bc2a012a984aada816029
Miguel Gomez <miguel.gomez@noirlab.edu>
2023-07-19 19:22:09 -0400

    new feat

--- app/acuicultura_app.py
+++ app/acuicultura_app.py
@@ -24,67 +24,44 @@ def set_horario():
     id_estanque = data.get('id_estanque')
     hora_inicio = data.get('hora_inicio')
     hora_termino = data.get('hora_termino')
+    luz = data.get('luz')
 
     # Comprobar si los valores son iguales a los almacenados en caché
     cache_key = f'horario_{id_estanque}'
     cached_horario = cache.get(cache_key)
 
-    if cached_horario and cached_horario.get('hora_inicio') == hora_inicio and cached_horario.get('hora_termino') == hora_termino:
-        return ("El horario es el mismo, no se realizó ningún cambio")
+    if cached_horario and cached_horario.get('hora_inicio') == hora_inicio and cached_horario.get('hora_termino') == hora_termino and cached_horario.get('luz') == luz:
+        return "El horario y el estado de luz son los mismos, no se realizó ningún cambio"
 
-    now = datetime.now().strftime("%H:%M")
-    now = to_minutes(now)
-    before = to_minutes(hora_inicio)
-    after = to_minutes(hora_termino)
-    print(before, now , after)
+    before = datetime.strftime(hora_inicio,"%H:%M")
+    after = datetime.strftime(hora_termino,"%H:%M")
 
-    if (before < after):
-        gpio.named_output("AC_LIGHT",before < now and now < after)
-    else:
-        gpio.named_output("AC_LIGHT",now < after or before < now)
 
+    if (before > after):
+        hora_inicio,hora_termino=hora_termino,hora_inicio
+        luz = luz!="True"
+
+    gpio.named_output("AC_LIGHT", luz=="True")
     cursor = connection.cursor()
-    cursor.execute("UPDATE estanque SET hora_encendido = %s, hora_apagado = %s WHERE id_estanque = %s", (hora_inicio, hora_termino, id_estanque))
+    cursor.execute("UPDATE estanque SET hora_encendido = %s, hora_apagado = %s, luz_encendida= %s WHERE id_estanque = %s", (hora_inicio, hora_termino, luz, id_estanque))
     connection.commit()
 
     # Actualizar los valores en caché
-    cache.set(cache_key, {'hora_inicio': hora_inicio, 'hora_termino': hora_termino})
+    cache.set(cache_key, {'hora_inicio': hora_inicio, 'hora_termino': hora_termino, 'luz': luz})
 
     print('Se cambió el horario de:', id_estanque)
-    return "Horario actualizado"
+    return "Horario y estado de luz actualizados"
 
-@app.route('/set/luz', methods=['POST'])
-def set_luz():
-    data = request.json
-    id_estanque = data.get('id_estanque')
-    luz = data.get('luz')
+# @app.route('/set/medicion', methods=['POST'])
+# def set_medicion():
+#     id_estanque = request.json.get('id_estanque') #Obtiene el id_estanque que hace la medicion
+#     data = CA.enviarEstadoAgua() #Obtiene array con Datetime, turbiedad, anomalía, luz en una lista
+#     cursor = connection.cursor()
+#     cursor.execute("INSERT INTO medicion (id_estanque, luz, nivel_turbiedad, nivel_maduracion, anomalia, fecha_medicion) VALUES (%s, %s, %s, %s, %s, %s)", (id_estanque, data[3], data[1], data[1], data[2], data[0]))
+#     connection.commit()
 
-    # Comprobar si el valor es igual al almacenado en caché
-    cache_key = f'luz_{id_estanque}'
-    cached_luz = cache.get(cache_key)
-
-    if cached_luz == luz:
-        return "La luz es la misma, no se realizó ningún cambio"
-    gpio.named_output("AC_LIGHT", luz=="True")
-    cursor = connection.cursor()
-    cursor.execute("UPDATE estanque SET luz_encendida = %s WHERE id_estanque = %s", (luz, id_estanque))
-    connection.commit()
-    # Actualizar el valor en caché
-    cache.set(cache_key, luz)
-
-    print('Se cambió la luz de:', id_estanque)
-    return "Luz actualizada"
-
-@app.route('/set/medicion', methods=['POST'])
-def set_medicion():
-    id_estanque = request.json.get('id_estanque') #Obtiene el id_estanque que hace la medicion
-    data = CA.enviarEstadoAgua() #Obtiene array con Datetime, turbiedad, anomalía, luz en una lista
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO medicion (id_estanque, luz, nivel_turbiedad, nivel_maduracion, anomalia, fecha_medicion) VALUES (%s, %s, %s, %s, %s, %s)", (id_estanque, data[3], data[1], data[1], data[2], data[0]))
-    connection.commit()
-
-    print("Se realizo medicion con los datos: ", data)
-    return "Medición realizada"
+#     print("Se realizo medicion con los datos: ", data)
+#     return "Medición realizada"
 
 if __name__ == '__main__':
     if connection is not None:
