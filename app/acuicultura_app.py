d541bceb48f57b3b2a1a2ba6b6de368c3e90cf00
Miguel Gomez <miguel.gomez@noirlab.edu>
2023-07-19 20:59:35 -0400

    ahora si que si 3

--- app/acuicultura_app.py
+++ app/acuicultura_app.py
@@ -35,6 +35,7 @@ def set_horario():
 
     if (before > after):
         hora_inicio,hora_termino=hora_termino,hora_inicio
+        estado_luz=not estado_luz
 
     if (before < now and now < after and luz == "True"):
         gpio.named_output("AC_LIGHT", estado_luz)
